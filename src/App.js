a82e9e9f4b7bc99002de9587c3027e9d1953c42b
FrancaTM <francatm@outlook.com>
2019-10-09 23:31:05 -0300

    add some styled components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from "react";
 import "./App.css";
+import styled from "styled-components";
 
 import UserSummary from "./components/UserSummary";
 
+const StyledDiv = styled.div`
+  width: 90%;
+  max-width: 40rem;
+  margin: 2rem auto;
+  border: 1px solid #ccc;
+  padding: 1rem;
+`;
+
 function App() {
   return (
-    <div
-      style={{
-        width: "90%",
-        maxWidth: "40rem",
-        margin: "2rem auto",
-        border: "1px solid #ccc",
-        padding: "1rem"
-      }}
-    >
+    <StyledDiv>
       <UserSummary />
-    </div>
+    </StyledDiv>
   );
 }
 
